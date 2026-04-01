84aebd238e30fd26733f48dd045193b97df9c658
Kamil Klamra <kamilklamra1@gmail.com>
2026-04-01 21:04:06 +0200

    lekkie niedociągnięcie

--- server.py
+++ server.py
@@ -1,7 +1,9 @@
+import os
 from flask import Flask, request, jsonify, send_from_directory
 from flask_cors import CORS
 import oracledb
-import os
+
+os.environ["NLS_LANG"] = ".AL32UTF8"
 
 app = Flask(__name__)
 CORS(app)
